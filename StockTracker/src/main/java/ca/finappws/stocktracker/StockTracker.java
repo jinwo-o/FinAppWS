6907f61cdc2761ab7654e905aa71d09e977b7097
Jin <jinwoo.hwang99@gmail.com>
2020-10-06 03:02:43 -0400

    Finished up the models

--- StockTracker/src/main/java/ca/finappws/stocktracker/StockTracker.java
+++ StockTracker/src/main/java/ca/finappws/stocktracker/StockTracker.java
@@ -1,13 +1,13 @@
 package ca.finappws.stocktracker;
 
-// import ca.finappws.stocktracker.readers.ReadCSV;
-// import ca.finappws.stocktracker.model.Stock;
-// import ca.finappws.stocktracker.model.Portfolio;
+import ca.finappws.stocktracker.readers.ReadCSV;
+import ca.finappws.stocktracker.model.Stock;
+import ca.finappws.stocktracker.model.Portfolio;
 import java.io.IOException;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+// import java.io.FileReader;
 // import com.opencsv.CSVWriter;
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 
 //
 public class StockTracker {
@@ -16,33 +16,31 @@ public class StockTracker {
         System.out.println("ABC");
 
         String path = "/Users/jin/Work/FinAppWS/stocks.csv";
-        String line = "";
+        // String line = "";
 
-        try {
-            System.out.println("We here");
-            BufferedReader br = new BufferedReader(new FileReader(path));
+        // try {
+        //     System.out.println("We here");
+        //     BufferedReader br = new BufferedReader(new FileReader(path));
 
-            while((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                System.out.println("Stock Name:" + values[0]);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        //     while((line = br.readLine()) != null) {
+        //         String[] values = line.split(",");
+        //         System.out.println("Stock Name:" + values[0]);
+        //     }
+        // } catch (FileNotFoundException e) {
+        //     e.printStackTrace();
+        // }
         // } catch (IOException e) {
         //     e.printStackTrace();
         // }
 
-        // CSVWriter writer = new CSVWriter(new FileWriter("stocks.csv"));
+        Portfolio portfolio = ReadCSV.CSV_Reader(path);
+        double totalValue = 0;
+        for(Stock stock : portfolio.getStocks()) {
+            double stockTotalValue = stock.getNumOfShares();
+            totalValue = stockTotalValue + totalValue;
+        }
 
-        // Portfolio portfolio = ReadCSV.CSV_Reader("/users/jin/work/FinAppWS/stocks.csv");
-        // double totalValue = 0;
-        // for(Stock stock : portfolio.getStocks()) {
-        //     double stockTotalValue = stock.getTotalVal();
-        //     totalValue = stockTotalValue + totalValue;
-        // }
-
-        // System.out.println("Total value of portfolio: "+ totalValue);
+        System.out.println("Total value of portfolio: "+ totalValue);
     }
 
 }
--- StockTracker/src/main/java/ca/finappws/stocktracker/readers/ReadCSV.java
+++ StockTracker/src/main/java/ca/finappws/stocktracker/readers/ReadCSV.java
@@ -1,45 +1,73 @@
-// package ca.finappws.stocktracker.readers;
+package ca.finappws.stocktracker.readers;
 
-// import ca.finappws.stocktracker.model.Stock;
-// // import ca.fantasybasketball.teamweeklystats.model.Roster;
-// // import ca.fantasybasketball.teamweeklystats.model.Stats;
-// import com.opencsv.CSVParser;
-// import com.opencsv.CSVParserBuilder;
-// import com.opencsv.CSVReader;
-// import com.opencsv.CSVReaderBuilder;
+import ca.finappws.stocktracker.model.Stock;
+import ca.finappws.stocktracker.model.Portfolio;
+// import ca.fantasybasketball.teamweeklystats.model.Roster;
+// import ca.fantasybasketball.teamweeklystats.model.Stats;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.io.StringReader;
-// import java.io.File;
+// Read the CSV File and enter the lines into
+// a roster
+public class ReadCSV {
 
-// // Read the CSV File and enter the lines into
-// // a roster
-// public class ReadCSV {
+    private ReadCSV() {
+    }
 
-//     private ReadCSV() {
-//     }
+    public static Portfolio CSV_Reader(String filename) throws IOException {
 
-//     public static Portfolio CSV_Reader(String filename) throws IOException {
+        String[] line;
+        Double d = 1.4;
+        Portfolio portfolio = new Portfolio();
 
-//         String[] line;
-//         Double d = 1.4;
-//         Portfolio portfolio = new Portfolio();
+        String word = "";
 
-//         final CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
-//         final CSVReader reader = new CSVReaderBuilder(new FileReader(filename)).withSkipLines(1).withCSVParser(parser).build();
-//         while ((line = reader.readNext()) != null) {
-//             Stock stock = new Stock();
-//             stock.setStockName(line[0]);
-//             stock.setSymbol(line[1]);
-//             stock.setNumOfShares(Integer.parseInt(line[2]));
-//             stock.setAvgPrice(d.parseDouble(line[3]));
-//             stock.setCurrentPrice(d.parseDouble(line[4]));
-//             stock.setTotalVal(d.parseDouble(line[5]));
-//             portfolio.getStocks().add(stock);
-//         }
-//         return portfolio;
-//     }
+        try {
+            // System.out.println("We here");
+            BufferedReader br = new BufferedReader(new FileReader(filename));
 
-// }
+            while((word = br.readLine()) != null) {
+                String[] values = word.split(",");
+                // Skip first line
+                if (values[0] == "Stock Name") {
+                    break;
+                }
+                System.out.println("Stock Name:" + values[0]);
+                Stock stock = new Stock();
+                stock.setStockName(values[0]);
+                stock.setSymbol(values[1]);
+                stock.setNumOfShares(Integer.parseInt(values[2]));
+                // stock.setAvgPrice(d.parseDouble(values[3]));
+                // stock.setCurrentPrice(d.parseDouble(values[4]));
+                // stock.setTotalVal(d.parseDouble(values[5]));
+                portfolio.getStocks().add(stock);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // final CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
+        // final CSVReader reader = new CSVReaderBuilder(new FileReader(filename)).withSkipLines(1).withCSVParser(parser).build();
+        // while ((line = reader.readNext()) != null) {
+        //     Stock stock = new Stock();
+        //     stock.setStockName(line[0]);
+        //     stock.setSymbol(line[1]);
+        //     stock.setNumOfShares(Integer.parseInt(line[2]));
+        //     stock.setAvgPrice(d.parseDouble(line[3]));
+        //     stock.setCurrentPrice(d.parseDouble(line[4]));
+        //     stock.setTotalVal(d.parseDouble(line[5]));
+        //     portfolio.getStocks().add(stock);
+        // }
+        return portfolio;
+    }
+
+}
